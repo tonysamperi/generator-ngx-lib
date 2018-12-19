3dedb9087d4bdbd243a3f2f94cf8b26613b7b405
Tony Samperi <tonysamperibg89@gmail.com>
2018-12-19 11:04:02 +0100

    v 1.0.2
    * Fixes #2
    * Refactor

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,222 +6,223 @@ const underscoreString = require("underscore.string");
 const Generator = require("yeoman-generator");
 
 var toCamelCase = function (string) {
-  return string.replace(/(\-\w)/g, function (matches) {
-    return matches[1].toUpperCase();
-  });
+    return string.replace(/(\-\w)/g, function (matches) {
+        return matches[1].toUpperCase();
+    });
 };
 
 var toPascalCase = function (string) {
-  var firstLetter = string.charAt(0);
-  return firstLetter.toUpperCase() + toCamelCase(string.slice(1));
+    var firstLetter = string.charAt(0);
+    return firstLetter.toUpperCase() + toCamelCase(string.slice(1));
 };
 
 module.exports = class extends Generator {
 
-  constructor(args, opts) {
-    super(args, opts);
-  }
-
-  initializing() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      "Welcome to the " + chalk.red("NgxLibGen") + " generator!"
-    ));
-  }
-
-  prompting() {
-    const prompts = [
-      {
-        type: "input",
-        name: "authorName",
-        message: "Your full name:",
-        validate: function (input) {
-          if (/.+/.test(input)) {
-            return true;
-          }
-          return "Please enter your full name";
-        },
-        default: this.user.git.name
-      },
-      {
-        type: "input",
-        name: "authorEmail",
-        message: "Your email address:",
-        validate: function (input) {
-          if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(input)) {
-            return true;
-          }
-          return "Please enter a valid email address";
-        },
-        default: this.user.git.email
-      },
-      {
-        type: "input",
-        name: "libraryName",
-        message: "Your library name (kebab-case)",
-        default: underscoreString.slugify(this.appname),
-        filter: function (x) {
-          return underscoreString.slugify(x);
-        }
-      },
-      {
-        type: "input",
-        name: "scope",
-        message: "Your library scope (eg: @angular) leave blank for none",
-        default: "",
-        validate: function (x) {
-          return !x || x.indexOf("@") === 0;
-        },
-        filter: function (x) {
-          return x ? x + "/" : "";
-        }
-      },
-      {
-        type: "input",
-        name: "angularVersion",
-        message: "Angular version (5 / 6 / 7):",
-        validate: function (input) {
-          if (/^[5|6|7]$/.test(input)) {
-            return true;
-          }
-          return "Please enter a valid angular version!";
-        },
-        default: 7
-      },
-      {
-        type: "input",
-        name: "gitRepositoryUrl",
-        message: "Git repository url",
-        default: "https://github.com/johndoe/my-cool-lib",
-        store: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-
-      this.props = {
-        author: {
-          name: props.authorName,
-          email: props.authorEmail
-        },
-        libraryName: {
-          original: props.libraryName,
-          kebabCase: props.libraryName,
-          camelCase: toCamelCase(props.libraryName),
-          pascalCase: toPascalCase(props.libraryName)
-        },
-        gitRepositoryUrl: props.gitRepositoryUrl,
-        angularVersion: parseInt(props.angularVersion),
-        scope: props.scope
-      };
-
-    });
-  }
-
-  writing() {
-
-
-    this.fs.copyTpl(
-      this.templatePath("_package_" + this.props.angularVersion + ".json"),
-      this.destinationPath("package.json"),
-      {
-        props: this.props
-      }
-    );
-
-    if (this.props.angularVersion === 5) {
-      this.fs.copyTpl(
-        this.templatePath("_angular-cli.json"),
-        this.destinationPath(".angular-cli.json"),
-        {
-          props: this.props
-        }
-      );
-    }
-    else {
-      this.fs.copyTpl(
-        this.templatePath("_angular.json"),
-        this.destinationPath("angular.json"),
-        {
-          props: this.props
-        }
-      );
+    constructor(args, opts) {
+        super(args, opts);
     }
 
-    // Copy directories
-
-    this.fs.copyTpl(
-      this.templatePath("_environments/**/*"),
-      this.destinationPath("environments")
-    );
-
-    this.fs.copyTpl(
-      this.templatePath("_lib/**/*"),
-      this.destinationPath("lib")
-    );
-
-    this.fs.copyTpl(
-      this.templatePath("_styles/empty.scss"),
-      this.destinationPath("lib/scss/" + this.props.libraryName.kebabCase + ".scss")
-    );
-
-    this.fs.copyTpl(
-      this.templatePath("_src/**/*"),
-      this.destinationPath("src"),
-      {
-        props: this.props
-      }
-    );
-
-    // Clear root scss
-    if (this.props.angularVersion === 5) {
-      this.fs.copyTpl(
-        this.templatePath("_styles/root.scss"),
-        this.destinationPath("src/styles.scss"),
-        {
-          props: this.props
-        }
-      );
-    }
-    else {
-      this.fs.copyTpl(
-        this.templatePath("_styles/root.scss"),
-        this.destinationPath("src/main.scss"),
-        {
-          props: this.props
-        }
-      );
+    initializing() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            "Welcome to the " + chalk.red("NgxLibGen") + " generator!"
+        ));
     }
 
-    // Templates with different destination than source
-    var otherFiles = [
-      {src: "_gulpfile.js", dest: "gulpfile.js"},
-      {src: "_README.md", dest: "README.md"},
-      {src: "_changelog.md", dest: "changelog.md"},
-      {src: "_gitignore", dest: "ng-package.json"},
-      {src: "_travis.yml", dest: ".travis.yml"},
-      {src: "_tsconfig.json", dest: "tsconfig.json"},
-      {src: "_tslint.json", dest: "tslint.json"},
-      {src: "_tsrefs/_public_api.ts", dest: "lib/public_api.ts"},
-      {src: "_tsrefs/_sample.module.ts", dest: "lib/" + this.props.libraryName.kebabCase + ".module.ts"},
-      {src: "_tsrefs/_app.module.ts", dest: "src/app/app.module.ts"}
-    ];
+    prompting() {
+        const prompts = [
+            {
+                type: "input",
+                name: "authorName",
+                message: "Your full name:",
+                validate: function (input) {
+                    if (/.+/.test(input)) {
+                        return true;
+                    }
+                    return "Please enter your full name";
+                },
+                default: this.user.git.name
+            },
+            {
+                type: "input",
+                name: "authorEmail",
+                message: "Your email address:",
+                validate: function (input) {
+                    if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(input)) {
+                        return true;
+                    }
+                    return "Please enter a valid email address";
+                },
+                default: this.user.git.email
+            },
+            {
+                type: "input",
+                name: "libraryName",
+                message: "Your library name (kebab-case)",
+                default: underscoreString.slugify(this.appname),
+                filter: function (x) {
+                    return underscoreString.slugify(x);
+                }
+            },
+            {
+                type: "input",
+                name: "scope",
+                message: "Your library scope (eg: @angular) leave blank for none",
+                default: "",
+                validate: function (x) {
+                    return !x || x.indexOf("@") === 0;
+                },
+                filter: function (x) {
+                    return x ? x + "/" : "";
+                }
+            },
+            {
+                type: "input",
+                name: "angularVersion",
+                message: "Angular version (5 / 6 / 7):",
+                validate: function (input) {
+                    if (/^[5|6|7]$/.test(input)) {
+                        return true;
+                    }
+                    return "Please enter a valid angular version!";
+                },
+                default: 7
+            },
+            {
+                type: "input",
+                name: "gitRepositoryUrl",
+                message: "Git repository url",
+                default: "https://github.com/johndoe/my-cool-lib",
+                store: true
+            }
+        ];
 
-    // Copy otherFiles
-    for (var i = 0; i < otherFiles.length; i++) {
-      var file = otherFiles[i];
-      this.fs.copyTpl(
-        this.templatePath(file.src),
-        this.destinationPath(file.dest),
-        {
-          props: this.props
-        }
-      );
+        return this.prompt(prompts).then(props => {
+
+            this.props = {
+                author: {
+                    name: props.authorName,
+                    email: props.authorEmail
+                },
+                libraryName: {
+                    original: props.libraryName,
+                    kebabCase: props.libraryName,
+                    camelCase: toCamelCase(props.libraryName),
+                    pascalCase: toPascalCase(props.libraryName)
+                },
+                gitRepositoryUrl: props.gitRepositoryUrl,
+                angularVersion: parseInt(props.angularVersion),
+                scope: props.scope
+            };
+
+        });
     }
 
-  }
+    writing() {
 
-  install() {
-    this.installDependencies({bower: false});
-  }
+
+        this.fs.copyTpl(
+            this.templatePath("_package_" + this.props.angularVersion + ".json"),
+            this.destinationPath("package.json"),
+            {
+                props: this.props
+            }
+        );
+
+        if (this.props.angularVersion === 5) {
+            this.fs.copyTpl(
+                this.templatePath("_angular-cli.json"),
+                this.destinationPath(".angular-cli.json"),
+                {
+                    props: this.props
+                }
+            );
+        }
+        else {
+            this.fs.copyTpl(
+                this.templatePath("_angular.json"),
+                this.destinationPath("angular.json"),
+                {
+                    props: this.props
+                }
+            );
+        }
+
+        // Copy directories
+
+        this.fs.copyTpl(
+            this.templatePath("_environments/**/*"),
+            this.destinationPath("environments")
+        );
+
+        this.fs.copyTpl(
+            this.templatePath("_lib/**/*"),
+            this.destinationPath("lib")
+        );
+
+        this.fs.copyTpl(
+            this.templatePath("_styles/empty.scss"),
+            this.destinationPath("lib/scss/" + this.props.libraryName.kebabCase + ".scss")
+        );
+
+        this.fs.copyTpl(
+            this.templatePath("_src/**/*"),
+            this.destinationPath("src"),
+            {
+                props: this.props
+            }
+        );
+
+        // Clear root scss
+        if (this.props.angularVersion === 5) {
+            this.fs.copyTpl(
+                this.templatePath("_styles/root.scss"),
+                this.destinationPath("src/styles.scss"),
+                {
+                    props: this.props
+                }
+            );
+        }
+        else {
+            this.fs.copyTpl(
+                this.templatePath("_styles/root.scss"),
+                this.destinationPath("src/main.scss"),
+                {
+                    props: this.props
+                }
+            );
+        }
+
+        // Templates with different destination than source
+        var otherFiles = [
+            {src: "_gulpfile.js", dest: "gulpfile.js"},
+            {src: "_README.md", dest: "README.md"},
+            {src: "_changelog.md", dest: "changelog.md"},
+            {src: "_gitignore", dest: "ng-package.json"},
+            {src: "_travis.yml", dest: ".travis.yml"},
+            {src: "_gitignore", dest: ".gitignore"},
+            {src: "_tsconfig.json", dest: "tsconfig.json"},
+            {src: "_tslint.json", dest: "tslint.json"},
+            {src: "_tsrefs/_public_api.ts", dest: "lib/public_api.ts"},
+            {src: "_tsrefs/_sample.module.ts", dest: "lib/" + this.props.libraryName.kebabCase + ".module.ts"},
+            {src: "_tsrefs/_app.module.ts", dest: "src/app/app.module.ts"}
+        ];
+
+        // Copy otherFiles
+        for (var i = 0; i < otherFiles.length; i++) {
+            var file = otherFiles[i];
+            this.fs.copyTpl(
+                this.templatePath(file.src),
+                this.destinationPath(file.dest),
+                {
+                    props: this.props
+                }
+            );
+        }
+
+    }
+
+    install() {
+        this.installDependencies({bower: false});
+    }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,41 @@
 "use strict";
-var path = require("path");
+
 var gulp = require("gulp");
-var eslint = require("gulp-eslint");
-var excludeGitignore = require("gulp-exclude-gitignore");
 var mocha = require("gulp-mocha");
 var istanbul = require("gulp-istanbul");
 var plumber = require("gulp-plumber");
-var coveralls = require("gulp-coveralls");
 var exec = require("child_process").exec;
 
 gulp.task("audit", function (cb) {
-	exec("npm audit --audit-level high", function (err, stdout, stderr) {
-		console.info("Audit complete!");
-		cb(err);
-	});
+    exec("npm audit --audit-level high", function (err, stdout, stderr) {
+        console.info("Audit complete!");
+        cb(err);
+    });
 });
 
 gulp.task("pre-test", function () {
-  return gulp.src("generators/**/*.js")
+    return gulp.src("generators/**/*.js")
     .pipe(istanbul({
-      includeUntested: true
+        includeUntested: true
     }))
     .pipe(istanbul.hookRequire());
 });
 
 gulp.task("test", ["pre-test"], function (cb) {
-  var mochaErr;
+    var mochaErr;
 
-  gulp.src("test/**/*.js")
+    gulp.src("test/**/*.js")
     .pipe(plumber())
     .pipe(mocha({reporter: "spec", timeout: 60000}))
     .on("error", function (err) {
-      mochaErr = err;
+        mochaErr = err;
     })
     .pipe(istanbul.writeReports())
     .on("end", function () {
-      cb(mochaErr);
+        cb(mochaErr);
     });
 });
 
-gulp.task("coveralls", ["test"], function () {
-  if (!process.env.CI) {
-    return;
-  }
-
-  return gulp.src(path.join(__dirname, "coverage/lcov.info"))
-    .pipe(coveralls());
-});
 
 gulp.task("prepublish", ["test"]);
 gulp.task("default", ["test"]);
